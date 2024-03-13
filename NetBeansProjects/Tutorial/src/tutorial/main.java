dec786035bd8ce0953854c28241d8a8438748b6d
Adrian Pinzon <a.pinzon@correo.unimet.edu.ve>
2024-03-13 17:08:20 -0400

    nodo

---
+++ NetBeansProjects/Tutorial/src/tutorial/main.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tutorial;
+
+/**
+ *
+ * @author eduardopinzon
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /* The code below will print the words Hello World
+        to the screen, and it is amazing */
+        System.out.println("Hello world"); // This is a comment
+        
+        String name = "Api";
+        System.out.println(name);
+        
+        int num = 23;
+        num = 20;
+        System.out.println(num);
+        
+        float decimal = 25.55f;
+        System.out.println(decimal);
+        
+        char letter = 'A';
+        System.out.println(letter);
+        
+        boolean bool = false;
+        System.out.println(bool);
+        
+        String myname = "Apinzon";
+        System.out.println("Hello " + myname);
+        
+        String fullName = name + " " + myname;
+        System.out.println(fullName);
+        
+        int x = 5;
+        int y = 6;
+        System.out.println(x + y);
+
+        int i, j, k;
+        i = j = k = 50;
+        System.out.println(i + j + k);
+        
+        //The general rules for naming variables are:
+
+        //Names can contain letters, digits, underscores, and dollar signs
+        //Names must begin with a letter
+        //Names should start with a lowercase letter, and cannot contain whitespace
+        //Names can also begin with $ and _ (but we will not use it in this tutorial)
+        //Names are case-sensitive ("myVar" and "myvar" are different variables)
+        //Reserved words (like Java keywords, such as int or boolean) cannot be used as names
+        
+        
+        byte myNum = 100;
+        System.out.println(myNum);
+        
+        long numero = 15000000000L;
+        System.out.println(numero);
+        
+
+
+
+        
+
+    }
+    
+}
