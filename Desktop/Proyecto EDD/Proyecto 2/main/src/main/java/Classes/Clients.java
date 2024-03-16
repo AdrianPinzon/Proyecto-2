35376fa10d6d01303123de5fe1f75354e21d76d3
Adrian Pinzon <a.pinzon@correo.unimet.edu.ve>
2024-03-16 12:27:09 -0400

    Clases

---
+++ Desktop/Proyecto EDD/Proyecto 2/main/src/main/java/Classes/Clients.java
@@ -0,0 +1,17 @@
+package Classes;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author eduardopinzon
+ */
+public class Clients {
+    String nombre;
+    String apellido;
+    int habitacion;
+    String key = nombre + apellido;
+}
